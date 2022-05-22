4569d87d0bab2cb19d346631052404e560064309
abdalhalemshabalout <halimsamoglu136@gmail.com>
2022-05-22 20:48:49 +0300

    ana sayfadaki bulunan formu tasarladim ve ogrenci giris sayfasina kontrol ettim

--- index.php
+++ index.php
@@ -22,6 +22,115 @@
     <!-- //online-fonts -->
 </head>
 <body>
+    <!-- banner -->
+<div class="main_section_agile" id="home">
+	<div class="agileits_w3layouts_banner_nav">
+		<nav class="navbar navbar-default">
+			<div class="navbar-header navbar-left">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Gezinmeyi aç/kapat</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			<h1><a class="navbar-brand" href="index.html"><i class="fa fa-leanpub" aria-hidden="true"></i>SoftWare</a></h1>
+
+			</div>
+			<div class="w3layouts_header_right">
+			    <form action="#" method="post">
+					<input name="Search here" type="search" placeholder="Ara" required="">
+					<input type="submit" value="">
+				</form>
+			</div>
+			<ul class="agile_forms">
+				<li><a class="active" href="students/index.php"><i class="fa fa-sign-in" aria-hidden="true"></i> Giriş Yap</a> </li>
+			<!--	<li><a href="#" data-toggle="modal" data-target="#myModal3"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Sign Up</a> </li> -->
+			</ul>
+			
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+				<nav class="link-effect-2" id="link-effect-2">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="index.html" class="effect-3">Main</a></li>
+						<li><a href="#about" class="effect-3 scroll">Hakkımızda</a></li>
+						<li><a href="#services" class="effect-3 scroll">Hizmetler</a></li>
+						<li><a href="#gallery" class="effect-3 scroll">Bölümler</a></li>
+						<li><a href="#team" class="effect-3 scroll">Ekip</a></li>
+						<li><a href="#mail" class="effect-3 scroll">iletişim</a></li>
+					</ul>
+				</nav>
+
+			</div>
+		</nav>	
+		<div class="clearfix"> </div> 
+	</div>
+</div>
+
+<div class="about-bottom">
+	<div class="col-md-6 w3l_about_bottom_left">
+		<div class="video-grid-single-page-agileits">
+			<div data-video="NwNZSTHvOKk" id="video"> <img src="./images/ben2.jpg" alt="" class="img-responsive" /> </div>
+		</div>
+		<div class="w3l_about_bottom_left_video">
+			<h4> Biz Kimiz ... !</h4>
+		</div>
+	</div>
+	<div class="col-md-6 w3l_about_bottom_right one">
+		<div class="abt-w3l">
+			<div class="header-w3l">
+				<h2>Kabul formu</h2>
+				<h4>Aşağıdaki Bilgileri Giriniz</h4>
+				<form action="#" method="post" class="mod2">
+					<div class="col-md-6 col-xs-6 w3l-left-mk">
+						<ul>
+							<li class="text">Adınız :  </li>
+							<li class="agileits-main"><i class="fa fa-user-o" aria-hidden="true"></i><input name="name" type="text" required=""></li>
+							<li class="text">Doğum tarihi :  </li>
+							<li class="agileits-main"><i class="fa fa-calendar" aria-hidden="true"></i><input class="date" id="datepicker" name="Text" type="text" value="" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'mm/dd/yyyy';}" required="" /></li>
+							<li class="text">Baba Adı  :  </li>
+							<li class="agileits-main"><i class="fa fa-user-o" aria-hidden="true"></i><input name="name" type="text" required=""></li>
+							<li class="text">Cinsiyet  :  </li>
+							<li class="agileits-main"><i class="fa fa-user-o" aria-hidden="true"></i><input name="gender" type="text" required=""></li>
+						</ul>
+					</div>
+					<div class="col-md-6 col-xs-6 w3l-right-mk">
+						<ul>
+							<li class="text">telefon numarası  :  </li>
+							<li class="agileits-main"><i class="fa fa-phone" aria-hidden="true"></i><input name="mobile" type="text" required=""></li>
+							<li class="text">ilçe :  </li>
+							<li class="agileits-main"><i class="fa fa-home" aria-hidden="true"></i><input name="address" type="text" required=""></li>
+							<li class="text">Mahalle  :  </li>
+							<li class="agileits-main"><i class="fa fa-map-marker" aria-hidden="true"></i><input name="address" type="text" required=""></li>
+							<li class="text">Cadde  :  </li>
+							<li class="agileits-main"><i class="fa fa-map-marker" aria-hidden="true"></i><input name="address" type="text" required=""></li>
+						</ul>
+					</div>
+					<div class="clearfix"></div>
+					<div class="agile-submit">
+						<input type="submit" value="Gönder">
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- //banner -->
+
+	
+<!-- Modal -->
+<div class="modal fade" id="myModal3" tabindex="-1" role="dialog">
+	<div class="modal-dialog">
+	<!-- Modal content-->
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="clearfix"> </div> 
+<!-- //Modal -->
+
     
 </body>
 </html>
--- setting/config.php
+++ setting/config.php
@@ -7,6 +7,21 @@ class project2
 	public $password = "";
 	public $dbname = "march9";
 	public $connectdb;
+
+	function __construct()
+	{
+		$this->connectdb = new mysqli($this->server,$this->username,$this->password,$this->dbname);
+		if($this->connectdb->connect_error)
+		{
+			die("connection failed");
+		}
+	}
+	
+    public function hackme()
+    {
+        $this->connectdb = new mysqli($this->server,$this->username,$this->password,$this->dbname);
+        return $this->connectdb;
+    }
 }
 
 $ravi = new project2;
