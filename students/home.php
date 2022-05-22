a7cfd4a517b3c268906d72680678ad1cfb6ddf61
amedridvan <ahmedridvan1@gmail.com>
2022-05-22 23:55:53 +0300

    back end kısmı tasarlandı .

--- students/home.php
+++ students/home.php
@@ -16,3 +16,81 @@ else
 
 
 ?>
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<title>Öğrenci</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content="Augment Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+	<script type="application/x-javascript">
+		addEventListener("load", function() {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!-- Bootstrap Core CSS -->
+	<link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+	<!-- Custom CSS -->
+	<link href="css/style_1.css" rel='stylesheet' type='text/css' />
+	<!-- Graph CSS -->
+	<link href="css/font-awesome.css" rel="stylesheet">
+	<!-- jQuery -->
+	<link href='//fonts.googleapis.com/css?family=Roboto:700,500,300,100italic,100,400' rel='stylesheet' type='text/css'>
+	<!-- lined-icons -->
+	<link rel="stylesheet" href="css/icon-font.min.css" type='text/css' />
+	<!-- //lined-icons -->
+	<script src="js/jquery-1.10.2.min.js"></script>
+	<script src="js/amcharts.js"></script>
+	<script src="js/serial.js"></script>
+	<script src="js/light.js"></script>
+	<script src="js/radar.js"></script>
+	<link href="css/barChart.css" rel='stylesheet' type='text/css' />
+	<link href="css/fabochart.css" rel='stylesheet' type='text/css' />
+	<!--clock init-->
+	<script src="js/css3clock.js"></script>
+	<!--Easy Pie Chart-->
+	<!--skycons-icons-->
+	<script src="js/skycons.js"></script>
+
+	<script src="js/jquery.easydropdown.js"></script>
+
+	<!--//skycons-icons-->
+</head>
+
+<body>
+	<div class="page-container">
+		<!--/content-inner-->
+		<div class="left-content">
+			<div class="inner-content">
+				<!-- header-starts -->
+				<div class="header-section">
+
+					<div class="clearfix"></div>
+				</div>
+				<!-- //header-ends -->
+				<div class="outter-wp">
+					<!--/tabs-->
+					<div class="tab-main">
+						<!--/tabs-inner-->
+						<div class="tab-inner">
+							<div id="tabs" class="tabs">
+								<h2 class="inner-tittle">Merhaba,
+									<?php echo ucfirst($student_name_display['st_fullname']); ?> </h2>
+								<div class="graph">
+									<nav>
+										<ul>
+											<li><a href="#section-1" class="icon-shop"><i class="lnr lnr-briefcase"></i> <span>Bilgiler</span></a></li>
+											<li><a href="#section-2" class="icon-cup"><i class="lnr lnr-lighter"></i> <span>Şifre değiştirme</span></a></li>
+											<li><a href="#section-3" class="icon-food"><i class="fa fa-user-circle-o" aria-hidden="true"></i> <span>öğretmenler</span></a></li>
+											<li><a href="#section-4" class="icon-lab"><i class="fa fa-flask"></i> <span>Dersler</span></a></li>
+											<li><a href="#section-5" class="icon-truck"> <span>Sonuçlar</span></a></li>
+										</ul>
+									</nav>
+								</div>
+	</body>
