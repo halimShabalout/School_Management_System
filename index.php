f3eab02e6ff023ce8816db7cd5f4f4ff9b29f547
abdalhalemshabalout <halimsamoglu136@gmail.com>
2022-05-31 21:43:31 +0300

    ana sayfada kalan kısmı tamamlandı

--- index.php
+++ index.php
@@ -711,5 +711,57 @@ jQuery(function($) {
 <!-- //Calendar -->	
 
 <!-- //js-scripts -->
+
+<!-- smooth scrolling -->
+<script src="js/SmoothScroll.min.js"></script>
+<!-- //smooth scrolling -->
+<!-- stats -->
+<script type="text/javascript" src="js/numscroller-1.0.js"></script>
+<!-- //stats -->
+<!-- moving-top scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+	<script type="text/javascript">
+		$(document).ready(function() {								
+		$().UItoTop({ easingType: 'easeOutQuart' });
+		});
+	</script>
+	<a href="#home" class="scroll" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+<!-- //moving-top scrolling -->
+
+<!-- gallery popup -->
+<script src="js/jquery.swipebox.min.js"></script> 
+<script type="text/javascript">
+jQuery(function($) {
+	$(".swipebox").swipebox();
+});
+</script>
+<!-- //gallery popup -->
+<!--/script-->
+	<script src="js/simplePlayer.js"></script>
+			<script>
+				$("document").ready(function() {
+					$("#video").simplePlayer();
+				});
+			</script>
+<!-- //Baneer-js -->
+<!-- Calendar -->
+<script src="js/jquery-ui.js"></script>
+	<script>
+	  $(function() {
+		$( "#datepicker" ).datepicker();
+	 });
+	</script>
+<!-- //Calendar -->	
+
+<!-- //js-scripts -->
 </body>
 </html>
