b69298a3eea8dae0280ea7083ec9fc1c440a35b0
abdalhalemshabalout <halimsamoglu136@gmail.com>
2022-06-07 18:06:50 +0300

    admin check controlu yapildi

--- setting/config.php
+++ setting/config.php
@@ -22,6 +22,7 @@ class project2
         $this->connectdb = new mysqli($this->server,$this->username,$this->password,$this->dbname);
         return $this->connectdb;
     }
+
 	public function student_login_check($st_username,$st_password)
 	{ 
 		$st_login_check = "SELECT  * from st_info where st_username = '$st_username' and st_password='$st_password'";
@@ -29,6 +30,7 @@ class project2
 		$st_login_num = $st_login_run->num_rows;
 		return $st_login_num;
 	}
+
 	public function student_info_select($st_username)
 	{
 		$student_info_sel = "SELECT * from st_info where st_username='$st_username'";
@@ -36,6 +38,20 @@ class project2
 		
 		return $student_info_run;
 	}
+
+		/////////////////////////////// ADMINNNNNNNNNNNNNNNNN--------------------------
+	
+		public function meadmin_check($admin_username,$admin_password)
+		{
+			$meadin_login_select = "SELECT * from meadmin where admin_username='$admin_username' AND admin_password='$admin_password'";
+			$meadmin_login_run = $this->connectdb->query($meadin_login_select);
+			// var_dump($meadmin_login_run);
+			if($meadmin_login_run){
+			$meadmin_login_num = $meadmin_login_run->num_rows;
+			return $meadmin_login_num;
+			}
+			
+		}
 }
 
 $ravi = new project2;
