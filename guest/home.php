723e0fd0cf924c6eaee844e1718f2309f581efb2
amedridvan <ahmedridvan1@gmail.com>
2022-06-23 00:06:43 +0300

    Misafir için ana sayfa yapıldı.

---
+++ guest/home.php
@@ -0,0 +1,64 @@
+<?php 
+include "../setting/config.php";
+ session_start();
+if(!$_SESSION['st_user'])
+{
+	
+	header("location:index.php");
+}
+else
+{
+	$st_username = $_SESSION['st_user'];
+	$st_name = $ravi->student_info_select($st_username);
+	
+	$student_name_display = $st_name->fetch_assoc();
+}
+
+
+?>
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<title>Misafir</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content="Augment Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+	<script type="application/x-javascript">
+		addEventListener("load", function() {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!-- Bootstrap Core CSS -->
+	<link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+	<!-- Custom CSS -->
+	<link href="css/style_1.css" rel='stylesheet' type='text/css' />
+	<!-- Graph CSS -->
+	<link href="css/font-awesome.css" rel="stylesheet">
+	<!-- jQuery -->
+	<link href='//fonts.googleapis.com/css?family=Roboto:700,500,300,100italic,100,400' rel='stylesheet' type='text/css'>
+	<!-- lined-icons -->
+	<link rel="stylesheet" href="css/icon-font.min.css" type='text/css' />
+	<!-- //lined-icons -->
+	<script src="js/jquery-1.10.2.min.js"></script>
+	<script src="js/amcharts.js"></script>
+	<script src="js/serial.js"></script>
+	<script src="js/light.js"></script>
+	<script src="js/radar.js"></script>
+	<link href="css/barChart.css" rel='stylesheet' type='text/css' />
+	<link href="css/fabochart.css" rel='stylesheet' type='text/css' />
+	<!--clock init-->
+	<script src="js/css3clock.js"></script>
+	<!--Easy Pie Chart-->
+	<!--skycons-icons-->
+	<script src="js/skycons.js"></script>
+
+	<script src="js/jquery.easydropdown.js"></script>
+
+	<!--//skycons-icons-->
+</head>
