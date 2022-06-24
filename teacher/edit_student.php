43ed4213f2720690f8b0bc89148a3b900a0d5cef
abdalhalemshabalout <halimsamoglu136@gmail.com>
2022-06-24 17:48:27 +0300

    Öğrenci güncelle frontend kısmı yapıldı

--- teacher/edit_student.php
+++ teacher/edit_student.php
@@ -2,6 +2,7 @@
 	<!--sub-heard-part-->
 	<div class="sub-heard-part">
 		<ol class="breadcrumb m-b-0">
+            <li><a href="index.html">Ana Sayfa</a></li>
 			<li class="active">
 				<?php if(isset($_GET['ravi'])){ echo strtoupper($page=$_GET['ravi']); } ?>
 			</li>
@@ -13,7 +14,26 @@
 			<?php echo strtoupper($_GET['ravi']); ?>
 		</h2>
 
-	
+        <div class="tables">
+
+
+                    <table class="table table-bordered ">
+
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Adı</th>
+                                <th>Adres</th>
+                                <th>Email</th>
+                                <th>Soyad</th>
+                                <th>babab</th>
+                                <th>anne</th>
+                                <th>Doğum Tarihi</th>
+                                <th>İletişim</th>
+                                <th>Cinsiyet</th>
+                            </tr>
+                        </thead>
+                        <tbody>
 
 						<?php $student_info_teacher = $ravi->teacher_info_display_admin();
 													$t_sn = 1;
@@ -54,7 +74,7 @@
 							</td>
 							<td>
 								
-				<a href="home.php?ravi=student-editnow&studentid=<?php echo $student_info_teacher['t_id']; ?>" class="btn red"></a>
+				<a href="home.php?ravi=student-editnow&studentid=<?php echo $student_info_teacher['t_id']; ?>" class="btn red">Düzenle</a>
 					</td>
 						</tr>
 						<?php $t_sn++; } ?>
@@ -63,7 +83,7 @@
 				</table>
 
 			</div>
-
+        </div>
 
 	</div>
 	<!--//graph-visual-->
