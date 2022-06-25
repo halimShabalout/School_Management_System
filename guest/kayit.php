6d17dc3c0e89de55830868987dc113853a276168
amedridvan <ahmedridvan1@gmail.com>
2022-06-25 20:46:52 +0300

    Misafir için kayıt formu

---
+++ guest/kayit.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>kayit formu</title>
+    <!-- Meta tag Keywords -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Scholarly web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+    function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <!--// Meta tag Keywords -->
+    <!-- css files -->
+    <link rel="stylesheet" href="css/bootstrap.css"> <!-- Bootstrap-Core-CSS -->
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="all" /> <!-- Style-CSS --> 
+    <link rel="stylesheet" href="css/font-awesome.css"> <!-- Font-Awesome-Icons-CSS -->
+    <link rel="stylesheet" href="css/swipebox.css">
+    <link rel="stylesheet" href="css/jquery-ui.css" />
+    <!-- //css files -->
+    <!-- online-fonts -->
+    <link href="//fonts.googleapis.com/css?family=Exo+2:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=cyrillic,latin-ext" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext" rel="stylesheet">
+    <!-- //online-fonts -->
+</head>
+<body>
+<div class="col-md-6 w3l_about_bottom_right one">
+		<div class="abt-w3l">
+			<div class="header-w3l">
+				<h2>Kabul formu</h2>
+				<h4>Aşağıdaki Bilgileri Giriniz</h4>
+				<form action="#" method="post" class="mod2">
+					<div class="col-md-6 col-xs-6 w3l-left-mk">
+						<ul>
+							<li class="text">Adınız :  </li>
+							<li class="agileits-main"><i class="fa fa-user-o" aria-hidden="true"></i><input name="name" type="text" required=""></li>
+							<li class="text">Doğum tarihi :  </li>
+							<li class="agileits-main"><i class="fa fa-calendar" aria-hidden="true"></i><input class="date" id="datepicker" name="Text" type="text" value="" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'mm/dd/yyyy';}" required="" /></li>
+							<li class="text">Baba Adı  :  </li>
+							<li class="agileits-main"><i class="fa fa-user-o" aria-hidden="true"></i><input name="name" type="text" required=""></li>
+							<li class="text">Cinsiyet  :  </li>
+							<li class="agileits-main"><i class="fa fa-user-o" aria-hidden="true"></i><input name="gender" type="text" required=""></li>
+						</ul>
+					</div>
+					<div class="col-md-6 col-xs-6 w3l-right-mk">
+						<ul>
+							<li class="text">telefon numarası  :  </li>
+							<li class="agileits-main"><i class="fa fa-phone" aria-hidden="true"></i><input name="mobile" type="text" required=""></li>
+							<li class="text">ilçe :  </li>
+							<li class="agileits-main"><i class="fa fa-home" aria-hidden="true"></i><input name="address" type="text" required=""></li>
+							<li class="text">Mahalle  :  </li>
+							<li class="agileits-main"><i class="fa fa-map-marker" aria-hidden="true"></i><input name="address" type="text" required=""></li>
+							<li class="text">Cadde  :  </li>
+							<li class="agileits-main"><i class="fa fa-map-marker" aria-hidden="true"></i><input name="address" type="text" required=""></li>
+						</ul>
+					</div>
+					<div class="clearfix"></div>
+					<div class="agile-submit">
+						<input type="submit" value="Gönder">
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div> 
+<!-- //banner -->
+</body>
+</html>
