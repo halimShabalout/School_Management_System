9360c7d9dc583f43e431b516250282c5185065f7
amedridvan <ahmedridvan1@gmail.com>
2022-06-26 20:51:30 +0300

    Misafir için iletişim sayfası

---
+++ guest/iletisim.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>kayit formu</title>
+    <!-- Meta tag Keywords -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Scholarly web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+    function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <!--// Meta tag Keywords -->
+    <!-- css files -->
+    <link rel="stylesheet" href="css/bootstrap.css"> <!-- Bootstrap-Core-CSS -->
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="all" /> <!-- Style-CSS --> 
+    <link rel="stylesheet" href="css/font-awesome.css"> <!-- Font-Awesome-Icons-CSS -->
+    <link rel="stylesheet" href="css/swipebox.css">
+    <link rel="stylesheet" href="css/jquery-ui.css" />
+    <!-- //css files -->
+    <!-- online-fonts -->
+    <link href="//fonts.googleapis.com/css?family=Exo+2:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=cyrillic,latin-ext" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext" rel="stylesheet">
+    <!-- //online-fonts -->
+</head>
+<body>
+<div id="mail" class="contact">
+	<div class="container">
+		<h3 class="w3l-title">Mail Us</h3>
+		<div class="w3layouts_header">
+			<p><i class="fa fa-graduation-cap" aria-hidden="true"></i></p>
+		</div>
+		<div class="agile_banner_bottom_grids">
+			<div class="col-md-4 col-xs-4 w3_agile_contact_grid">
+				<div class="agile_contact_grid_left">
+					<i class="fa fa-map-marker" aria-hidden="true"></i>
+				</div>
+				<div class="agile_contact_grid_right agilew3_contact">
+					<h4>Adress</h4>
+					<p><?php echo $general_fetch['website_address']; ?></p>
+					<p>Nepal</p>
+				</div>
+			</div>
+			<div class="col-md-4 col-xs-4 w3_agile_contact_grid">
+				<div class="agile_contact_grid_left agileits_w3layouts_left">
+					<i class="fa fa-mobile" aria-hidden="true"></i>
+				</div>
+				<div class="agile_contact_grid_right agileits_w3layouts_right">
+					<h4>Telefon Numarası</h4>
+					<p><?php echo $general_fetch['website_phone1']; ?> <span><?php echo $general_fetch['website_phone2']; ?></span></p>
+				</div>
+			</div>
+			<div class="col-md-4 col-xs-4 w3_agile_contact_grid">
+				<div class="agile_contact_grid_left agileits_w3layouts_left1">
+					 <i class="fa fa-envelope-o" aria-hidden="true"></i>
+				</div>
+				<div class="agile_contact_grid_right agileits_w3layouts_right1">
+					<h4>Email</h4>
+					<p><a href="mailto:info@example.com"><?php echo $general_fetch['website_email1']; ?></a>
+						<span><a href="mailto:info@example.com"><?php echo $general_fetch['website_email2']; ?></a></span></p>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+    </body>
+</html>    
