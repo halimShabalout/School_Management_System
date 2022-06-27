39d06a5b13785b6603842d939acee5a2b5253114
abdalhalemshabalout <halimsamoglu136@gmail.com>
2022-06-27 15:23:43 +0300

    Öğretmen dashboard / sad bar tasarlandı ve backend kısmını yazıldı

--- teacher/home.php
+++ teacher/home.php
@@ -65,6 +65,112 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 
 </head>
 <body>
+	
+<body>
+	<div class="page-container">
+		<!--/content-inner-->
+		<div class="left-content">
+			<div class="inner-content">
+				<!-- header-starts -->
+				<div class="header-section">
+
+					<div class="clearfix"></div>
+				</div>
+				<!-- //header-ends -->
+
+				<?php 
+							
+				$homepage = "home1";
+				if(isset($_GET['ravi']))
+				{
+					$homepage = $_GET['ravi'];
+				}
+				include $homepage.".php";
+						
+				?>				
+				<!--footer section start-->
+				<footer>
+					<p> <a href="http://yaz.mf.firat.edu.tr/tr" target="_blank">SOFTWAER Fırat Üniversitesi  Yazılım Mühendisliği </a></p>
+				</footer>
+				<!--footer section end-->
+				
+			</div>
+		</div>
+		<!--//content-inner-->
+		<!--/sidebar-menu-->
+		<div class="sidebar-menu">
+			<header class="logo">
+				<a href="#" class="sidebar-icon"> <span class="fa fa-bars"></span> </a> <a href="index.html"> <span id="logo"> <h1>Software</h1></span> 
+					<!--<img id="logo" src="" alt="Logo"/>--> 
+				  </a>
+			</header>
+			<div style="border-top:1px solid rgba(69, 74, 84, 0.7)"></div>
+			<!--/down-->
+			<div class="down">
+				<a href="index.html"><img src="images/admin.jpg"></a>
+				<a href="index.php"><span class=" name-caret"><?php echo $info_display['t_fullname']; ?></span></a>
+				<p>Öğretmen</p>
+				<ul>
+					<li><a class="tooltips" href="index.html"><span>Profil</span><i class="lnr lnr-user"></i></a></li>
+					<li><a class="tooltips" href="index.html"><span>Ayarlar</span><i class="lnr lnr-cog"></i></a></li>
+					<li><a class="tooltips" href="logouts.php"><span>Çıkış Yap</span><i class="lnr lnr-power-switch"></i></a></li>
+				</ul>
+			</div>
+			<!--//down-->
+			<div class="menu">
+				<ul id="menu">
+					<li><a href="home.php"><i class="fa fa-tachometer"></i> <span>Dashboard</span></a></li>
+					<li id="menu-academico"><a href="#"><i class="fa fa-table"></i> <span>Öğrenciler</span> <span class="fa fa-angle-right" style="float: right"></span></a>
+						<ul id="menu-academico-sub">
+							<li id="menu-academico-avaliacoes"><a href="home.php?ravi=student-information">Öğrenci Bilgileri</a></li>
+							<li id="menu-academico-boletim"><a href="home.php?ravi=add-student">Öğrenci Ekleme</a></li>
+							<li id="menu-academico-avaliacoes"><a href="calender.html">Öğrenci Silme</a></li>
+
+						</ul>
+					</li>
+											
+					<li id="menu-academico"><a href="#"><i class="fa fa-file-text-o"></i> <span>Ayarlar</span> <span class="fa fa-angle-right" style="float: right"></span></a>
+					<ul id="menu-academico-sub">
+						
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+	<script>
+		var toggle = true;
+
+		$(".sidebar-icon").click(function() {
+			if (toggle) {
+				$(".page-container").addClass("sidebar-collapsed").removeClass("sidebar-collapsed-back");
+				$("#menu span").css({
+					"position": "absolute"
+				});
+			} else {
+				$(".page-container").removeClass("sidebar-collapsed").addClass("sidebar-collapsed-back");
+				setTimeout(function() {
+					$("#menu span").css({
+						"position": "relative"
+					});
+				}, 400);
+			}
+
+			toggle = !toggle;
+		});
+	</script>
+	<!--js -->
+	<link rel="stylesheet" href="css/vroom.css">
+	<script type="text/javascript" src="js/vroom.js"></script>
+	<script type="text/javascript" src="js/TweenLite.min.js"></script>
+	<script type="text/javascript" src="js/CSSPlugin.min.js"></script>
+	<script src="js/jquery.nicescroll.js"></script>
+	<script src="js/scripts.js"></script>
+
+	<!-- Bootstrap Core JavaScript -->
+	<script src="js/bootstrap.min.js"></script>
+</body>
     
 </body>
 </html>
