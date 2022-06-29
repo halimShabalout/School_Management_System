bbe93303ee4c1dc2f05d8d12608e85c7fdd10afd
amedridvan <ahmedridvan1@gmail.com>
2022-06-29 17:41:38 +0300

    misafir hocları görme

---
+++ guest/hocalar.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>dersler </title>
+    <!-- Meta tag Keywords -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Scholarly web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+    function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <!--// Meta tag Keywords -->
+    <!-- css files -->
+    <link rel="stylesheet" href="css/bootstrap.css"> <!-- Bootstrap-Core-CSS -->
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="all" /> <!-- Style-CSS --> 
+    <link rel="stylesheet" href="css/font-awesome.css"> <!-- Font-Awesome-Icons-CSS -->
+    <link rel="stylesheet" href="css/swipebox.css">
+    <link rel="stylesheet" href="css/jquery-ui.css" />
+    <!-- //css files -->
+    <!-- online-fonts -->
+    <link href="//fonts.googleapis.com/css?family=Exo+2:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=cyrillic,latin-ext" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext" rel="stylesheet">
+    <!-- //online-fonts -->
+</head>
+<body>
+<div class="team-w3l" id="team">
+	<div class="container">
+		<h3 class="w3l-title">Hocalarımız</h3>
+		<div class="w3layouts_header">
+			<p><i class="fa fa-graduation-cap" aria-hidden="true"></i></p>
+		</div>
+		<div class="team-w3l-grid">
+			<div class="col-md-4 col-xs-4 about-poleft t1">
+				<div class="about_img"><img src="images/HT1.jpg" alt="">
+				  <h5>BİLAL ALATAŞ</h5>
+				  <div class="about_opa">
+					<p>Yapay Öğrenme</p>
+					<ul class="fb_icons2 text-center">
+						<li><a class="fa fa-facebook" href="#"></a></li>
+						<li><a class="fa fa-twitter" href="#"></a></li>
+						<li><a class="fa fa-google" href="#"></a></li>
+						<li><a class="fa fa-linkedin" href="#"></a></li>
+						<li><a class="fa fa-pinterest-p" href="#"></a></li>
+					</ul>
+				  </div>
+				</div>
+			</div>
+			<div class="col-md-4 col-xs-4 about-poleft t2">
+				<div class="about_img"><img src="images/HT2.jpg" alt="">
+				  <h5>Seda ARSLAN TUNCER</h5>
+				  <div class="about_opa">
+					<p> Görüntü İşleme</p>
+					<ul class="fb_icons2 text-center">
+						<li><a class="fa fa-facebook" href="#"></a></li>
+						<li><a class="fa fa-twitter" href="#"></a></li>
+						<li><a class="fa fa-google" href="#"></a></li>
+						<li><a class="fa fa-linkedin" href="#"></a></li>
+						<li><a class="fa fa-pinterest-p" href="#"></a></li>
+					</ul>
+				  </div>
+				</div>
+			</div>
+			<div class="col-md-4 col-xs-4 about-poleft t3">
+				<div class="about_img"><img src="images/HT3.jpeg" alt="">
+				  <h5>Mustafa ULAŞ</h5>
+				  <div class="about_opa">
+					<p>Big Data, Social Media</p>
+					<ul class="fb_icons2 text-center">
+						<li><a class="fa fa-facebook" href="#"></a></li>
+						<li><a class="fa fa-twitter" href="#"></a></li>
+						<li><a class="fa fa-google" href="#"></a></li>
+						<li><a class="fa fa-linkedin" href="#"></a></li>
+						<li><a class="fa fa-pinterest-p" href="#"></a></li>
+					</ul>
+				  </div>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+    </div>
+</div>
+</body>
+</html>
+        
